cb28114d07b78314060a0a4931593f47c100fbc1
Mario Garcia <mario.ggar@gmail.com>
2014-11-24 17:46:13 +0100

    Adding placeholders in build.gradle

--- templates/android-groovy-archetype/lazybones.groovy
+++ templates/android-groovy-archetype/lazybones.groovy
@@ -1,17 +1,38 @@
 
 import static org.apache.commons.io.FilenameUtils.concat
-
 import org.apache.commons.io.FileUtils
 
 def props = [:]
 
+// --------------------------------------------
+// --------------- QUESTIONS ------------------
+// --------------------------------------------
+
 props.projectName = projectDir.name
+
 props.defaultPackage =
     ask("Define value for default package [org.company.example]: ",
-        "org.company.example",
-        "defaultPackage")
+    "org.company.example",
+    "defaultPackage")
+props.minSdkVersion =
+    ask("Which is the minimum version of sdk you want to target ? [19.0.0]: ",
+    "19.0.0",
+    "minSdkVersion")
+props.targetSdkVersion =
+    ask("Which is the main version of SDK you are targeting ? [21]: ",
+    "21",
+    "targetSdkVersion")
+props.buildToolsVersion =
+    ask("Which version of Android Build Tools do you want to use ? [20.0.0]: ",
+    "20.0.0",
+    "buildToolsVersion")
+
+// --------------------------------------------
+// ----------- PROCESSING TEMPLATES -----------
+// --------------------------------------------
 
 processTemplates "README.md", props
+processTemplates "build.gradle", props
 processTemplates "**/strings.xml", props
 processTemplates "**/MainActivity.gtpl", props
 processTemplates "**/AndroidManifest.xml", props
@@ -25,5 +46,9 @@ def sourceDestination =
     )
 
 sourceDestination.parentFile.mkdirs()
+
+// --------------------------------------------
+// --------- MOVE FILES TO DESTINATION --------
+// --------------------------------------------
 
 FileUtils.moveFile(new File(templateDir, "MainActivity.gtpl"), sourceDestination)
